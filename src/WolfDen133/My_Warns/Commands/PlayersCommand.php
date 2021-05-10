918eaf40a4450eeb1c6cc02b3d4a0c92f689b0d6
WolfDen133 <61922721+WolfDen133@users.noreply.github.com>
2021-05-10 21:33:57 +0100

    Add files via upload

---
+++ src/WolfDen133/My_Warns/Commands/PlayersCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace WolfDen133\My_Warns\Commands;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\PluginIdentifiableCommand;
+use pocketmine\plugin\Plugin;
+use pocketmine\utils\TextFormat;
+use WolfDen133\My_Warns\Main;
+
+class PlayersCommand extends Command implements PluginIdentifiableCommand
+{
+    /** @var Main */
+    private $plugin;
+
+    public function __construct(Main $main)
+    {
+        parent::__construct("players");
+
+        $this->setUsage("Usage: /players");
+        $this->setPermissionMessage(TextFormat::RED . "Unknown command. Try /help for a list of commands");
+        $this->setPermission("mywarns.command.players");
+        $this->setDescription("See all the current logged on players");
+        $this->setAliases(["p"]);
+
+        $this->plugin = $main;
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args)
+    {
+        if ($sender->hasPermission($this->getPermission())) {
+
+            $players = [];
+            $ops = [];
+            foreach ($this->plugin->getServer()->getOnlinePlayers() as $player) {
+                $player->isOp() ? array_push($ops, $player->getName()) : array_push($players, $player->getName());
+            }
+
+            $sender->sendMessage("========================\nOnline players:\n" . TextFormat::AQUA . implode(TextFormat::RESET . ", " . TextFormat::AQUA, $players) . TextFormat::RESET . "\n\nOnline operators: \n"  . TextFormat::AQUA . implode(TextFormat::RESET. ", " . TextFormat::AQUA, $ops) . "\n========================");
+
+        } else {
+            $sender->sendMessage($this->getPermissionMessage());
+        }
+    }
+
+    public function getPlugin(): Plugin
+    {
+        return $this->plugin;
+    }
+}
---
+++ src/WolfDen133/My_Warns/Commands/RemoveWarnCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+
+namespace WolfDen133\My_Warns\Commands;
+
+
+use pocketmine\command\CommandSender;
+use pocketmine\command\PluginIdentifiableCommand;
+use pocketmine\plugin\Plugin;
+use pocketmine\utils\TextFormat;
+use WolfDen133\My_Warns\Main;
+use pocketmine\command\Command;
+
+class RemoveWarnCommand extends Command implements PluginIdentifiableCommand
+{
+    /** @var Main */
+    private $plugin;
+
+    public function __construct(Main $main)
+    {
+        parent::__construct("removewarn");
+
+        $this->setUsage("Usage: /removewarn <warnID: string>");
+        $this->setPermissionMessage(TextFormat::RED . "Unknown command. Try /help for a list of commands");
+        $this->setPermission("mywarns.command.removewarn");
+        $this->setDescription("Remove a false/old warn from a player");
+        $this->setAliases(["rwarn", "rmwarn", "removew"]);
+
+        $this->plugin = $main;
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args)
+    {
+        if ($sender->hasPermission($this->getPermission())) {
+            if (count($args) === 1) {
+                if ($this->plugin->hasWarn($args[0])) {
+                    $this->plugin->removeWarn($args[0]);
+                    $sender->sendMessage("Successfully removed warn ID: " . TextFormat::AQUA . $args[0]);
+                } else {
+                    $sender->sendMessage(TextFormat::RED . "There are no logs of that player having a warn with the ID: " . $args[0]);
+                }
+            } else {
+                $sender->sendMessage($this->getUsage());
+            }
+        } else {
+            $sender->sendMessage($this->getPermission());
+        }
+    }
+
+    public function getPlugin(): Plugin
+    {
+        return $this->plugin;
+    }
+}
